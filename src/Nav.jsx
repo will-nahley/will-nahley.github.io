a12cfa190cb4df854248d76846de3725a9fb865f
Will Nahley <wnahley@uchicago.edu>
2025-05-28 20:15:37 -0500

    Added icons for LinkedIn/Instagram

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import instaIcon from "../assets/instagram_icon.png";
+import linkedIcon from "../assets/linkedin_icon.png";
 
 export default function Nav() {
   return (
@@ -8,8 +10,12 @@ export default function Nav() {
       </div>
 
       <div className="navbar-right">
-        <a href="https://www.instagram.com/willnahley/">Instagram</a>
-        <a href="https://www.linkedin.com/in/will-nahley/">LinkedIn</a>
+        <a href="https://www.instagram.com/willnahley/">
+          <img className="icon" src={instaIcon} />
+        </a>
+        <a href="https://www.linkedin.com/in/will-nahley/">
+          <img className="icon" src={linkedIcon} />
+        </a>
         <a href="#resume">Resume</a>
         <a href="#projectsa">Projects</a>
         <a href="#contact">Contact</a>
